05f1512c9a87625eda03893fcb3dd81612b50fe1
unknown <fitrahulanwar95@gmail.com>
2023-03-28 16:24:33 +0700

    laravl persona

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Models\Category;
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home",
+        "active" => 'home'
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'title' => 'About',
+        'active' => 'about',
+        'name' => 'fitrah',
+        'email' => 'fitrahulanwar95@gmail.com',
+        'image' => 'fitra.jpg',
+    ]);
+});
+
+
